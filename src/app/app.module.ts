b486a88cd47bb03b72d334046797de8352b74847
Reshan Gayantha <reshangayantha9@gmail.com>
2023-06-15 19:39:38 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { NbThemeModule,NbLayoutModule,NbSidebarModule,NbSidebarService,NbMenuModule} from '@nebular/theme';
+import { NbIconModule } from '@nebular/theme';
+import {NavBarModule} from "./nav-bar/nav-bar.module";
+import {SideBarModule} from "./side-bar/side-bar.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {SideBarComponent} from "./side-bar/side-bar/side-bar.component";
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import {HomeModule} from "./home/home.module";
+import {RouterOutlet} from "@angular/router";
+import {AddNewModule} from "./add-new/add-new.module";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NbIconModule,
+    NbLayoutModule,
+    NbThemeModule.forRoot(),
+    NbMenuModule.forRoot(),
+    NbSidebarModule,
+    NavBarModule,
+    BrowserAnimationsModule,
+    SideBarModule,
+    NbEvaIconsModule,
+    RouterOutlet,
+    HomeModule,
+    AddNewModule
   ],
-  providers: [],
+  providers: [NbSidebarService,
+    {provide: SideBarComponent, useClass: SideBarComponent }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
